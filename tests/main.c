31a31110e6a8ad5623ac1f4997dc38c21139db43
aleksandarstojkovski <NadaRisto2>
2018-11-02 13:01:16 +0100

    tests

---
+++ tests/main.c
@@ -0,0 +1,12 @@
+//
+// Created by aleks on 02.11.2018.
+//
+#include <stdio.h>
+#include <math.h>
+#include <mem.h>
+
+int main() {
+    char * array[10];
+    char a;
+    printf("%d", sizeof(a));
+}

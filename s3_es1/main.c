191ded61efeda5ba56bf70ca2a620bdd15d9c3c3
aleksandarstojkovski <NadaRisto2>
2018-11-05 23:50:58 +0100

    fixed

--- s3_es1/main.c
+++ s3_es1/main.c
@@ -6,15 +6,19 @@
 #include <stdlib.h>
 #include <mem.h>
 
-int check_string_is_number(char str[]){
+// return 1 if number is not a string
+// return 0 if number is a string
+int string_is_number(char str[]){
     int number = atoi(str);
     if (number == 0 && str[0] != 0){
-        return 1;
+        return 0;
     }
-    return 0;
+    return 1;
 }
 
-int check_number_is_not_negative(char str[]){
+// return 1 if string is negative
+// return 0 if string is not negative
+int number_is_negative(char str[]){
     int i=0;
     while (str[i] != '\0'){
         if (str[i] == '-'){
@@ -25,26 +29,38 @@ int check_number_is_not_negative(char str[]){
     return 0;
 }
 
-/*
-int check_number_of_digits(char str[]){
+// Ex. 153 -> 1^3 + 5^3 + 3^3 = 153
+int check_number(char str[]){
+    int number=atoi(str);
     int i=0;
-    while (str[i] != '\0'){
-        i++;
+    int sum=0;
+
+    for (int i=0;i<strlen(str)-1;i++){
+        sum=sum+(str[i]-'0')*(str[i]-'0')*(str[i]-'0');
     }
-    return i-1;
+
+    if (sum == number){
+        return 1;
+    } else {
+        return 0;
+    }
 }
-*/
 
 int main(){
+
     char string[10];
     int number_of_characters;
 
     do{
         printf("Numero positivo di 3 cifre:");
         fgets(string, sizeof(string),stdin);
-    }while (strlen(string)-1 != 3 || check_number_is_not_negative(string) != 0 || check_string_is_number(string) != 0);
+    }while (strlen(string)-1 != 3 || number_is_negative(string) == 1 || string_is_number(string) != 1);
 
-   // check_number(string);
+    if (check_number(string) == 1){
+        printf("Risposta: Si");
+    } else {
+        printf("Risposta: No");
+    }
 
 }
 

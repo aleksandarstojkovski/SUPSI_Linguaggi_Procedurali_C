371d488ca57aad7384812606ecc3261bb3f51a71
Aleksandar Stojkovski <43863010+aleksandarstojkovski@users.noreply.github.com>
2019-01-06 11:17:06 +0100

    Update main.c

--- es_prep_1/main.c
+++ es_prep_1/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <memory.h>
 
 
 // 
 //				Es prep test
 //
 
+# define BUFFER_SIZE 50
+# define SENTINELLA 0
+
 static int index;
 static int INIT_LIST_SIZE = 5;
-static const long sentinella = 0;
-# define BUFFER_SIZE 50
 
 void print_list(long * list) {
 	for (int i = 0; i < index; i++) {
@@ -19,27 +21,32 @@ void print_list(long * list) {
 
 
 void delete_even_numbers(long * list) {
+	int * new_list = malloc(sizeof(long)*index);
+	int new_index = 0;
 	for (int i = 0; i < index; i++) {
 		if (list[i] % 2 == 0) {
-			for (int j = i; j < index - 1; j++) {
-				list[j] = list[j + 1];
-			}
-			index--;
+			list[i] = NULL;
 		}
 	}
+	for (int i = 0; i < index; i++) {
+		if (list[i] != NULL) {
+			new_list[new_index] = list[i];
+			new_index++;
+		}
+	}
+	memcpy(list, new_list, sizeof(long)*new_index);
+	index = new_index;
 }
 
-long * reverse_list(long * list) {
+void reverse_list(long * list) {
 	int * new_list = malloc(sizeof(long)*index);
 	for (int i = 0; i < index; i++) {
 		new_list[i] = list[index-i-1];
 	}
-	free(list);
-	return new_list;
+	memcpy(list, new_list, sizeof(long)*index);
 }
 
 void add_value_to_list(long value, long * list) {
-	//printf("index=%d, size=%d", index, INIT_LIST_SIZE);
 	if (index == INIT_LIST_SIZE) {
 		printf("\nLista ingrandita\n");
 		INIT_LIST_SIZE += 1;
@@ -62,17 +69,18 @@ int main() {
 	do {
 		fgets(buffer, BUFFER_SIZE, stdin);
 		value = strtol(buffer, NULL, 10);
-		if (value != sentinella) {
+		if (value != SENTINELLA) {
 			add_value_to_list(value, list);
 		}
 
-	} while (value != sentinella && index <= INIT_LIST_SIZE);
+	} while (value != SENTINELLA && index <= INIT_LIST_SIZE);
 
-	list = reverse_list(list);
+	reverse_list(list);
+	printf("\nlista riordinata: \n");
+	print_list(list);
+
 	delete_even_numbers(list);
-
-	printf("lista riordinata: \n");
-
+	printf("pari eleminiati: \n");
 	print_list(list);
 
 	printf("Fine - premi un tasto per chiudere!");
